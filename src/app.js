7bb7a63c4af407a3b63b79df182acfc2a2ac6a82
chab0114 <chab0114@algonquinlive.com>
2025-02-20 23:30:58 -0500

    set up Redis

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const omdbService = require('./services/omdbService');
+const redis = require('redis');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const redisClient = redis.createClient({
+    url: process.env.REDIS_URL
+});
+
+(async () => {
+    await redisClient.connect().catch(console.error);
+    console.log('Connected to Redis');
+})();
+  
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+    req.redisClient = redisClient;
+    next()
+});
 
 app.get('/', (req, res) => {
     res.send('Movie Tracker API is running');
@@ -18,9 +33,21 @@ app.get('/test-api/:title', async (req, res) => {
     } catch (error) {
       res.status(500).json({ error: error.message });
     }
-  });
+});  
+
+app.get('/test-redis', async (req, res) => {
+    try {
+      await req.redisClient.set('test', 'Redis is working!');
+      const value = await req.redisClient.get('test');
+      res.send(value);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
+
+
 
